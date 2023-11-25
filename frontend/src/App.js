0bccc8278af760ead0509370cd3ffe1c0e8bb3f7
Omar Bakr <104934837+OmarCodes2@users.noreply.github.com>
2023-11-25 17:55:43 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
-import Login from "./components/login";
+//import Login from "./components/login";
 import CreateAccount from "./components/createaccount";
-import Home from "./components/home";
+//import Home from "./components/home";
 
 function App() {
   const [data, setData] = useState(null);
