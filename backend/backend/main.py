08b2556bf2916360cd367d6e75ac66a02d3160b7
Bryson Wu <brysonwu@student.ubc.ca>
2023-11-24 22:21:09 -0500

    add basic root api behaviour

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -7,6 +7,10 @@ app = FastAPI()
 client = AsyncIOMotorClient('mongodb+srv://Admin:hackwestern@cluster0.v52qzqz.mongodb.net/')
 db = client.your_database_name
 
+@app.get("/")
+async def root():
+    return {'message': 'roll stangs'}
+
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     user = await db.users.find_one({"_id": user_id})
