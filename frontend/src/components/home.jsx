d2f8d2309b879accd267414fe660195d3be7d9bb
kittyyliu <kittyliu113@gmail.com>
2023-11-25 13:08:21 -0500

    styled homepage

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,32 +1,34 @@
-import React from 'react';
-import "./home.css";
+import React, { useState, useEffect } from 'react';
+import './home.css';
 import home from '../assets/home.png';
-import cat from '../assets/cat.png';
-import plant from '../assets/plant.png';
 
-const CreateAccount = () => {
-    return (
-        <div style={{ 
-            backgroundImage: `url(${home})`, 
-            backgroundSize: 'cover', 
+function Home() {
+  return (
+      <div
+        style={{
+          position: 'relative',
+          width: '100vw',
+          height: '100vh',
+          overflow: 'hidden',
+        }}
+      >
+        <div
+          style={{
+            backgroundImage: `url(${home})`,
+            backgroundSize: 'cover',
             backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat',
-            width: '100vw',
-            height: '100vh',
-            backgroundAttachment: 'fixed',
-            overflow: 'hidden'
-          }}>
-            <div className='container'>
-                <div className='pawndr'>Pawndr</div>
-                <div className='leftdiv'>Create your account:</div>
-            </div>
-            <div className='inputs'>
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+          }}
+        />
 
-            </div>
-
+        <div className="container">
+          <div className="pawndr">Pawndr</div>
         </div>
-    )
-
+      </div>
+  );
 }
 
-export default CreateAccount
+export default Home;
