3008a96444b37d39a6e1e4b18c15584348223510
Bryson Wu <brysonwu@student.ubc.ca>
2023-11-26 04:02:14 -0500

    implement voice replies

--- backend/backend/messaging.py
+++ backend/backend/messaging.py
@@ -4,10 +4,18 @@ from dotenv import load_dotenv
 from infobip_channels.sms.channel import SMSChannel
 from infobip_channels.whatsapp.channel import WhatsAppChannel
 
+import http.client
+from pydub import AudioSegment
+from pydub.playback import play
+import io
+
+from openai import OpenAI
+
 load_dotenv()
 
 BASE_URL = os.getenv('INFOBIP_URL')
 API_KEY = os.getenv('INFOBIP_KEY')
+OAI_KEY = os.getenv('OPEN_AI_API_KEY')
 SENDER = '12496638103'
 RECIPIENT = '17789298780'
 # RECIPIENT = '17783171775'
@@ -59,3 +67,30 @@ def whatsapp_bryson(msg: str):
     })
 
     return response
+
+def transcribe_audio(msg: dict):
+    c = WhatsAppChannel.from_auth_params({
+        'base_url': BASE_URL,
+        'api_key': API_KEY
+    })
+
+    conn = http.client.HTTPSConnection("mmxdyw.api.infobip.com")
+    payload = ''
+    headers = c.build_get_request_headers(API_KEY)
+    conn.request("GET", msg['results'][0]['message']['url'], payload, headers)
+    res = conn.getresponse()
+    data = res.read()
+
+    ogg = AudioSegment.from_ogg(io.BytesIO(data))
+
+    iomp3 = io.BytesIO()
+    iomp3.name = 'sound.mp3'
+    ogg.export(iomp3, format='mp3')
+
+    oai_client = OpenAI(api_key=OAI_KEY)
+    transcript = oai_client.audio.transcriptions.create(
+        model='whisper-1',
+        file=iomp3
+    )
+
+    return transcript.text
